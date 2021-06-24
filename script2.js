bff189e3759aa73af4b13a5392e3bcb42ffd27d9
isis-camila <83370439+isis-camila@users.noreply.github.com>
2021-06-23 23:41:07 -0400

    desafio cachipun listo

--- script2.js
+++ script2.js
@@ -3,102 +3,103 @@ let resultados = document.getElementById("resultados");
 let numeroJugadas = document.getElementById("numero-jugadas");
 let boton = document.getElementById("boton");
 let resultadosganador = document.getElementById("resultadosganador");
-let contador = 0;
 
-
+//jugadores
+let jugadora = 0;
+let jugadormaquina = 0;
+   
 boton.addEventListener("click", function(){
     let input = Math.floor(parseInt(numeroJugadas.value))
-  
-    
+        
     for(i=1; i<=input; i++){
         //random máquina
         let jugadamaquina = Math.floor(Math.random()*3 + 1);
-
-        //jugadores
-        let jugadora = 0;
-        let jugadorb = 0;
 
         //tipo de jugada
         var tipodejugada = parseInt(prompt("ingrese el tipo de jugada (número) que desea realizar, siendo 1 = Piedra, 2 = Papel y 3 = Tijeras"))
        
         switch (tipodejugada){
             case 1: //piedra, papel, tijera
-                if(tipodejugada == jugadamaquina){
+                if(tipodejugada === jugadamaquina){
                     alert("empataste")
                     resultados.innerHTML += `Empataste `
-                    jugadora++
-                    jugadorb++
+                    jugadora += 1
+                    jugadormaquina += 1
                 }
-                if(tipodejugada == 2){
+                if(jugadamaquina === 2){
                     alert("perdiste")
                     resultados.innerHTML += `PerdisTe `
-                    jugadorb++
+                    jugadormaquina += 1
                 }
-                if(tipodejugada == 3){
+                if(jugadamaquina === 3){
                     alert("ganaste")
                     resultados.innerHTML += `Ganaste `
-                    jugadora++
+                    jugadora += 1
                 }
                
             break;
             
             case 2: //piedra, papel, tijera
-                if(tipodejugada == jugadamaquina){
+                if(tipodejugada === jugadamaquina){
                     alert("empataste")
                     resultados.innerHTML += `Empataste `
-                    jugadora++
-                    jugadorb++
+                    jugadora += 1
+                    jugadormaquina += 1
                 }
-                if(tipodejugada == 3){
+                if(jugadamaquina === 3){
                     alert("perdiste")
                     resultados.innerHTML += `Perdiste `
-                    jugadorb++
+                    jugadormaquina += 1
                 }
-                if(tipodejugada == 1){
+                if(jugadamaquina === 1){
                     alert("ganaste")
                     resultados.innerHTML += `Ganaste `
-                    jugadora++
+                    jugadora += 1
                 }
             break;
             
             case 3: //piedra, papel, tijera
-                if(tipodejugada == jugadamaquina){
+                if(tipodejugada === jugadamaquina){
                     alert("empataste")
                     resultados.innerHTML += `Empataste `
-                    jugadora++
-                    jugadorb++
+                    jugadora += 1
+                    jugadormaquina += 1
                 }
-                if(tipodejugada == 1){
+                if(jugadamaquina === 1){
                     alert("perdiste")
                     resultados.innerHTML += `Perdiste `
-                    jugadorb++
+                    jugadormaquina += 1
                 }
-                if(tipodejugada == 2){
+                if(jugadamaquina === 2){
                     alert("ganaste")
                     resultados.innerHTML += `Ganaste `
-                    jugadora++
+                    jugadora += 1
                 }
             break;
         
             //validación
             default:
-                input--
+                input += 1
                 alert("tu jugada no corresponde a las opciones mencionadas 1= Piedra, 2 = Papel, 3 = Tijeras")
             
         }
 
-        // Definir ganador
-        
-        if(jugadora > jugadorb){
-            resultadosganador.innerHTML = `Ganaste la partida`
-        } if(jugadorb>jugadora){
-            resultadosganador.innerHTML = `Perdiste la partida`
-        } else{
-            resultadosganador.innerHTML = `Empataron`
-        }
-
     }
-    console.log(resultadosganador)
+
+// Definir ganador
+     
+    if(jugadora > jugadormaquina){
+        resultadosganador.innerHTML = `Ganaste la partida`
+    }else if(jugadormaquina > jugadora){
+        resultadosganador.innerHTML = `Perdiste la partida`
+    }else{
+        resultadosganador.innerHTML = `Empataron la partida`
+    }
+
+
+        console.log(jugadora, jugadormaquina)
+
 })
+
 
  
