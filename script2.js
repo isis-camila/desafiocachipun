2d32bc05b72da9c6d396f7aa07332f4408bc97ed
isis-camila <83370439+isis-camila@users.noreply.github.com>
2021-06-23 21:14:34 -0400

    arreglos 23.06

--- script2.js
+++ script2.js
@@ -5,87 +5,100 @@ let boton = document.getElementById("boton");
 let resultadosganador = document.getElementById("resultadosganador");
 let contador = 0;
 
+
 boton.addEventListener("click", function(){
     let input = Math.floor(parseInt(numeroJugadas.value))
+  
     
     for(i=1; i<=input; i++){
         //random máquina
         let jugadamaquina = Math.floor(Math.random()*3 + 1);
 
+        //jugadores
+        let jugadora = 0;
+        let jugadorb = 0;
+
         //tipo de jugada
         var tipodejugada = parseInt(prompt("ingrese el tipo de jugada (número) que desea realizar, siendo 1 = Piedra, 2 = Papel y 3 = Tijeras"))
-        
+       
         switch (tipodejugada){
-            case 1:
-                if(tipodejugada === jugadamaquina){
-                    alert('empataste')
-                    resultados.innerHTML += `Empataste`
+            case 1: //piedra, papel, tijera
+                if(tipodejugada == jugadamaquina){
+                    alert("empataste")
+                    resultados.innerHTML += `Empataste `
+                    jugadora++
+                    jugadorb++
                 }
-                if(tipodejugada === 2){
-                    alert('perdiste')
-                    resultados.innerHTML += `PerdisTe`
+                if(tipodejugada == 2){
+                    alert("perdiste")
+                    resultados.innerHTML += `PerdisTe `
+                    jugadorb++
                 }
-                if(tipodejugada === 3){
-                    alert('ganaste')
-                    resultados.innerHTML += `Ganaste`
+                if(tipodejugada == 3){
+                    alert("ganaste")
+                    resultados.innerHTML += `Ganaste `
+                    jugadora++
+                }
+               
+            break;
+            
+            case 2: //piedra, papel, tijera
+                if(tipodejugada == jugadamaquina){
+                    alert("empataste")
+                    resultados.innerHTML += `Empataste `
+                    jugadora++
+                    jugadorb++
+                }
+                if(tipodejugada == 3){
+                    alert("perdiste")
+                    resultados.innerHTML += `Perdiste `
+                    jugadorb++
+                }
+                if(tipodejugada == 1){
+                    alert("ganaste")
+                    resultados.innerHTML += `Ganaste `
+                    jugadora++
                 }
             break;
             
-            case 2:
-                if(tipodejugada === jugadamaquina){
-                    alert('empataste')
-                    resultados.innerHTML += `Empataste <br>`
+            case 3: //piedra, papel, tijera
+                if(tipodejugada == jugadamaquina){
+                    alert("empataste")
+                    resultados.innerHTML += `Empataste `
+                    jugadora++
+                    jugadorb++
                 }
-                if(tipodejugada === 1){
-                    alert('perdiste')
-                    resultados.innerHTML += `Perdiste <br>`
+                if(tipodejugada == 1){
+                    alert("perdiste")
+                    resultados.innerHTML += `Perdiste `
+                    jugadorb++
                 }
-                if(tipodejugada === 1){
-                    alert('ganaste')
-                    resultados.innerHTML += `Ganaste <br>`
-                }
-            break;
-            
-            case 3:
-                if(tipodejugada === jugadamaquina){
-                    alert('empataste')
-                    resultados.innerHTML += `Empataste <br>`
-                }
-                if(tipodejugada === 1){
-                    alert('perdiste')
-                    resultados.innerHTML += `Perdiste <br>`
-                }
-                if(tipodejugada === 2){
-                    alert('ganaste')
-                    resultados.innerHTML += `Ganaste <br> `
+                if(tipodejugada == 2){
+                    alert("ganaste")
+                    resultados.innerHTML += `Ganaste `
+                    jugadora++
                 }
             break;
         
             //validación
             default:
-                input +=1
+                input--
                 alert("tu jugada no corresponde a las opciones mencionadas 1= Piedra, 2 = Papel, 3 = Tijeras")
             
         }
+
+        // Definir ganador
+        
+        if(jugadora > jugadorb){
+            resultadosganador.innerHTML = `Ganaste la partida`
+        } if(jugadorb>jugadora){
+            resultadosganador.innerHTML = `Perdiste la partida`
+        } else{
+            resultadosganador.innerHTML = `Empataron`
+        }
+
     }
+    console.log(resultadosganador)
 })
 
-//define ganador
-while(contador < numeroJugadas){
-    
-    ganador = defineGanador(jugadas, jugadamaquina());
-    contador++;
-
-    switch (ganador) {
-      case 'humano':
-        alert(`¡Felicidades haz ganado!`)
-        resultadosganador.innerHTML = `¡Felicidades, ganaste!`
-        break;
-      case 'máquina':
-        alert(`haz perdido contra la máquina`)
-        resultadosganador.innerHTML = `¡Perdiste la ronda!`
-        break;
-      default:
-        alert('Empataste')
-    }
-}
+ 
